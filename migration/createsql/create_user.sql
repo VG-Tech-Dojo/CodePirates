ab0399656706fcf29bd84754ff83f6a0759a01c9
saruhei <saruhei19892046@gmail.com>
2013-03-15 18:06:13 +0900

    /Qusetion.phpに質問のid検索と質問を追加するメソッドを作成
    /lib/php/Db/Dao/Question.phpを作成し、
    insertメソッドとfindQuestionByIdメソッドとquestionListメソッドを作成した。
    modift migeration file & makefile
    
    Dao/Qusetion.phpに質問のid検索と質問を追加するメソッドを作成
    
    /lib/php/Db/Dao/Question.phpを作成し、
    insertメソッドとfindQuestionByIdメソッドとquestionListメソッドを作成した。
    
    table名のミスと、カラムのミスを修正

--- migration/createsql/create_user.sql
+++ migration/createsql/create_user.sql
@@ -2,9 +2,8 @@ CREATE  TABLE IF NOT EXISTS `question` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(50) NOT NULL ,
   `title` VARCHAR(50) NOT NULL ,
-  `content` TEXT NOT NULL ,
+  `content` TEXT NOT NULL,
   `created_at` DATETIME NOT NULL ,
   `updated_at` DATETIME NULL ,
-  PRIMARY KEY (`id`) 
-  UNIQUE (`content`) )
+  PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
