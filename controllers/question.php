363fefa7b7ba131e0e3957b8b15d52ca27ebd758
sinamon <Shihomi_Katayama@voyagegroup.com>
2013-03-19 16:18:47 +0900

    controller/question.phpにconfarmに渡す部分を作成

--- controllers/question.php
+++ controllers/question.php
@@ -15,7 +15,7 @@ $app->get('/question', 'noauthorized', function () use ($app) {
         echo $e->getMessage();
         $app->error('何も質問がありません'); 
     }
-    $app->render('question/questionList.twig',array('errors' => $errors, 'questionList' => $questionList));
+    $app->render('question/questionList.twig', array('errors' => $errors, 'questionList' => $questionList));
 });
 
 
@@ -31,11 +31,32 @@ $app->get('/question/:id', 'noauthorized', function ($id) use ($app) {
     try {
         $question_item = $question->getQuestionByID($id);
     } catch (PDOException $e){
+        echo $e->getMessage();
         $app->error('その問題は存在しません'); 
     }
-    $app->render('question/questionForm.twig',array('errors' => $errors, 'question' => $question_item));
+    $app->render('question/questionForm.twig', array('errors' => $errors, 'question' => $question_item));
 });
 
 
+
+/**
+ * 問題に回答した後の確認画面
+ */
+$app->post('/question/confarm', 'noauthorized', function () use ($app) {
+    require_once LIB_DIR . '/FormValidator/CodeFormValidator.php';
+    require_once MODELS_DIR . '/Answer.php';
+
+    $params = $app->request()->post();
+    $errors = array();
+    $form_validator = $app->factory->getFormValidator_CodeFormValidator();
+
+    if ($form_validator->run($params)) {
+        $confarmcode = $params['code'];
+    } else {
+        $confarmcode = '';
+        $errors = $form_validator->getErrors();
+    }
+    $app->render('question/confarm.twig', array('errors' => $errors, 'code' => $confarmcode, 'question_id' => $params['question_num']));
+});
 
 
