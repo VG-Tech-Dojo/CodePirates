87e371e2c11915a90c60f583bc29f1411f9bf4c9
sinamon <Shihomi_Katayama@voyagegroup.com>
2013-03-19 11:41:00 +0900

    回答フォームを表示するコントローラを作成

--- controllers/question.php
+++ controllers/question.php
@@ -3,7 +3,7 @@
 /**
  * 質問の一覧表示 
  */
-$app->get('/question/list', 'noauthorized', function () use ($app) {
+$app->get('/question', 'noauthorized', function () use ($app) {
     require_once MODELS_DIR . '/Question.php';
 
     $question = $app->factory->getQuestion();
@@ -12,7 +12,30 @@ $app->get('/question/list', 'noauthorized', function () use ($app) {
     try {
         $questionList = $question->getAllQuestion();
     } catch (PDOException $e){
+        echo $e->getMessage();
         $app->error('何も質問がありません'); 
     }
     $app->render('question/questionList.twig',array('errors' => $errors, 'questionList' => $questionList));
 });
+
+
+/**
+ * 問題と解答フォームの表示
+ */
+$app->get('/question/:id', 'noauthorized', function ($id) use ($app) {
+    require_once MODELS_DIR . '/Question.php';
+
+    $question = $app->factory->getQuestion();
+    $errors = array();
+    
+    try {
+        $question_item = $question->getQuestionByID($id);
+    } catch (PDOException $e){
+        $app->error('その問題は存在しません'); 
+    }
+    $app->render('question/questionForm.twig',array('errors' => $errors, 'question' => $question_item));
+});
+
+
+
+
