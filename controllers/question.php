7741f3b87a27712f4acebaf8034c592ffa276128
sinamon <Shihomi_Katayama@voyagegroup.com>
2013-03-21 13:37:18 +0900

    質問リストのエラーの付加
    questionコントローラの質問リスト表示時
    質問が一つもないときにエラーがでないようになっていたので、
    エラーを付け加えた。

--- controllers/question.php
+++ controllers/question.php
@@ -8,12 +8,14 @@ $app->get('/question', 'authorized', function () use ($app) {
 
     $question = $app->factory->getQuestion();
     $errors = array();
-    
+
     try {
-        $questionList = $question->getAllQuestion();
+        if (($questionList = $question->getAllQuestion()) == null) {
+            $errors = '質問がありません';
+        }
     } catch (PDOException $e){
         echo $e->getMessage();
-        $app->error('何も質問がありません'); 
+        $app->error('おかしいのでリロードしてください。'); 
     }
     $app->render('question/questionList.twig', array('errors' => $errors, 'questionList' => $questionList));
 });
@@ -32,7 +34,7 @@ $app->get('/question/:id', 'authorized', function ($id) use ($app) {
         $question_item = $question->getQuestionByID($id);
     } catch (PDOException $e){
         echo $e->getMessage();
-        $app->error('その問題は存在しません'); 
+        $app->error('おかしいのでリロードしてください'); 
     }
     $app->render('question/questionForm.twig', array('errors' => $errors, 'question' => $question_item));
 });
