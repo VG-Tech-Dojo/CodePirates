98849acfa365b4f25191f8dccb7d611d3aab4a07
saruhei <saruhei19892046@gmail.com>
2013-04-09 20:37:10 +0900

    いいね人数の表示

--- controllers/answer.php
+++ controllers/answer.php
@@ -53,9 +53,7 @@ $app->get('/answer/:a_id', 'authorized' ,function ($a_id) use ($app) {
     if (!($answer_comment = $comment->getCommentByAnsId($a_id))) {
         $answer_comment = "";    
     }
-    
-    $error_msg = $_SESSION['slim.flash'];
-    $app->render('answer/answer.twig', array('user' => $user_info, 'answer' => $answerInfo ,'question' => $questionInfo, 'answerer' => $answererInfo, 'comment' => $answer_comment, 'sessionid' => $sessionid, 'like' => $like, 'error_msg' => $error_msg ));
+    $app->render('answer/answer.twig', array('user' => $user_info, 'answer' => $answerInfo ,'question' => $questionInfo, 'answerer' => $answererInfo, 'comment' => $answer_comment, 'session' => $sessionid, 'like' => $like));
 });
 
 
@@ -101,12 +99,14 @@ $app->get('/answerlist/question/:id', 'authorized' ,  function ($q_id) use ($app
     require_once MODELS_DIR . '/Question.php';
     require_once MODELS_DIR . '/User.php';
     require_once MODELS_DIR . '/Comment.php';
+    require_once MODELS_DIR . '/Good.php';
 
 
     $session = $app->factory->getSession();
     $answer = $app->factory->getAnswer();
     $question = $app->factory->getQuestion();
     $comment = $app->factory->getComment();
+    $like = $app->factory->getGood();
     $user = $app->factory->getUser();
     $user_info = array();
     $answerInfos = array();
@@ -131,10 +131,16 @@ $app->get('/answerlist/question/:id', 'authorized' ,  function ($q_id) use ($app
             $answererInfo = array();
             $commentsinfo = $comment->getAllComments();
             $countForComment = array();
+            $likeInfo = $like->getAllLike();
+            $countForLike = array();
             for($i = 0; $i < count($commentsinfo); $i++){
                 $countForComment[] = $commentsinfo[$i]['a_id'];    
             }
             $countForComment = array_count_values($countForComment);
+            for($i = 0; $i < count($likeInfo); $i++){
+                $countForLike[] = $likeInfo[$i]['a_id'];    
+            }
+            $countForLike = array_count_values($countForLike);
 
             foreach($answerInfos as $answerInfo){
                 $user_data[] = $answerInfo['u_id'];
@@ -148,6 +154,11 @@ $app->get('/answerlist/question/:id', 'authorized' ,  function ($q_id) use ($app
                         $answerdata[$answerd_user]['answer'][$i]['comment'] = $countForComment[$answerdata[$answerd_user]['answer'][$i]['id']];
                     }else{
                         $answerdata[$answerd_user]['answer'][$i]['comment'] = 0;
+                    }
+                    if(array_key_exists($answerdata[$answerd_user]['answer'][$i]['id'], $countForLike)){
+                        $answerdata[$answerd_user]['answer'][$i]['like'] = $countForLike[$answerdata[$answerd_user]['answer'][$i]['id']];
+                    }else{
+                        $answerdata[$answerd_user]['answer'][$i]['like'] = 0;
                     }
                 }
             }
@@ -313,16 +324,13 @@ $app->post('/modify/answer/result', 'authorized', function () use ($app) {
 $app->post('/comment/register', 'authorized', function () use ($app) {
     require_once MODELS_DIR . '/Comment.php';
     require_once LIB_DIR . '/Session.php';
-    require_once LIB_DIR . '/FormValidator/CommentFormValidator.php';
 
     $params  = $app->request()->post();
     $session = $app->factory->getSession();
     $comment = $app->factory->getComment();
     $errors  = array();
-    $form_validator = $app->factory->getFormValidator_CommentFormValidator();
-    $user_info = array();
 
-    $session->remove('error_msg');
+    $user_info = array();
     if ($session->get('user_id')) {
         $user_info['id'] = $session->get('user_id');
         $user_info['name'] = $session->get('user_name');
@@ -331,28 +339,18 @@ $app->post('/comment/register', 'authorized', function () use ($app) {
         $app->redirect('/');
     }
     if($user_info != null && $params['sessionid'] === $session->get('sessionidA')){
-        
-        if ($form_validator->run($params)) {
-            $confarmcomment = $params['comment'];
-            try {
-                $comment->register(
-                    $user_info['id'],
-                    $params['answer_num'],
-                    $confarmcomment
-                );
-                $session->remove('sessionidA');
-
-            } catch (PDOException $e) {
-                print($e->getMessage());
-                $app->error('登録に失敗しました。');
-            }
-      
-        } else {
-            $confarmcomment = '';
-            $errors = $form_validator->getErrors();
-            $app->flash('error', $errors['comment']);
+        try {
+            $comment->register(
+                $user_info['id'],
+                $params['answer_num'],
+                $params['comment']
+            );
+            $session->remove('sessionidA');
+            
+        } catch (PDOException $e) {
+            print($e->getMessage());
+            $app->error('登録に失敗しました。');
         }
-
     }
 
     $app->redirect('/answer/'.$params['answer_num']);
