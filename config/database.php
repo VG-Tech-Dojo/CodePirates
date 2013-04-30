476b524df890bd5a84cc580d42cfb7a16c6a7eae
saruhei <saruhei19892046@gmail.com>
2013-04-30 17:41:01 +0900

    問題リストのソート後のデザインを変更

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$active_group = 'demo';
-$db = array();
-$db['demo'] = array();
-$db['demo']['dbname'] = 'demo';
-$db['demo']['hostname'] = 'localhost';
-$db['demo']['port'] = '3306';
-$db['demo']['user'] = 'demo';
-$db['demo']['password'] = 'demo';
